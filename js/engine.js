2af2fa7cdc2a510d91287f6262e3f97d4802dc30
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-11-15 21:01:47 +0100

    added text for "hello" and tutorial

--- js/engine.js
+++ js/engine.js
@@ -77,24 +77,57 @@ module.exports = __webpack_require__(1);
 "use strict";
 
 
+var firstMenu = __webpack_require__(2); //podstawowe funkcj
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log("NIEWIERNE PSY RULEZ!!!!");
 
+  //ukrywanie odpowiednich części
   $("header p, #heroBtns div").hide();
 
+  //przejście z pierwszego intro do pierwszego menu
   setTimeout(function () {
     $("header p, #heroBtns div").fadeIn(750);
     $("body").fadeIn(750).css("background", "beige");
+    $("#mainPart").empty().html(firstMenu.textHello);
   }, 16000);
 
+  //funkcje dla przycisków pierwszego menu
   $("#info").on("click", function () {
-    $("#mainPart").empty().html("<p class='standardText newRocker'>To kolejna wersja gry Audaces, w której wprowadzone zostały wszystkie nie zrealizowane wcześniej pomysły. Gra powstała dzięki użytym technologiom: HTML5, SASS, jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com.</p>");
+    firstMenu.infoBtn();
   });
-
   $("#licence").on("click", function () {
-    $("#mainPart").empty().html("<p class='standardText newRocker'>UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji).</p><p class='standardText newRocker'>POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI.</p><p class='standardText newRocker'>Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobbystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audaces jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie może w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.</p>");
+    firstMenu.licenceBtn();
+  });
+  $("#tutorial").on("click", function () {
+    firstMenu.tutorialBtn();
   });
 }); //koniec DOMContentLoaded
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+//tekst powitalny
+module.exports.textHello = "<p class='standardText newRocker'>Witaj w grze 'Audaces - serce z żelaza', pierwszej grze tekstowej Niewiernych Psów. Powyżej znajdują się cztery przyciski, dzięki którym poznasz podsatwowe informacje o tej wersji gry, licencji gry, samouczka obsługi interfejsu oraz rozpoczniesz grę AUDACES - serce z żelaza. Także tego. Studio Niewiernych Psów, życzy miłej zabawy.</p>";
+
+//przycisk info
+module.exports.infoBtn = function () {
+  $("#mainPart").empty().html("<p class='standardText newRocker'>To kolejna wersja gry Audaces, w której wprowadzone zostały wszystkie nie zrealizowane wcześniej pomysły. Gra powstała dzięki użytym technologiom: HTML5, SASS, jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com. Dostępne rozdzielczości: 1366x768 px.</p>");
+};
+
+//przycisk licencja
+module.exports.licenceBtn = function () {
+  $("#mainPart").empty().html("<p class='standardText newRocker'>UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji).</p><p class='standardText newRocker'>POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI.</p><p class='standardText newRocker'>Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobbystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audaces jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie może w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.</p>");
+};
+
+//przycisk tutorial
+module.exports.tutorialBtn = function () {
+  $("#mainPart").empty().html("<p class='standardText newRocker'>Początek gry. Ekran składa się z powitania oraz czterech przycisków. Po wciśnięciu przycisku Info, Licencja lub Tutorial obok pojawi się odpowiednia informacja. Po wciśnięciu przycisku Nowa gra, użytkownik rozpocznie grę. Wtedy będzie można stworzyć lub wylosować swoją postać. Po tym, gracz przejdze do gry. Ekran będzie podzielony na część z przyciskami - głównymi oraz interakcji. W głównej części będzie pojawiał się opis danej lokacji. W części komunikatów będą pojawiać się informacje o dokonanych wyborach oraz dodatkowe inforamcje związane z grą. Część z przyciskami będzie posiadała przyciski umożliwiające dostęp do cech postaci, ekwipunku, umiejętności itd. oraz do opcji, które będzie mógł wybrać w trakcie gry. Na podstawie opisów z głównej części gry, gracz będzie mógł dokonywać wyborów z listy przycisków.</p>");
+};
 
 /***/ })
 /******/ ]);
--- js/workingFile.js
+++ js/workingFile.js
@@ -1,21 +1,21 @@
+let firstMenu = require("./firstMenu.js"); //podstawowe funkcj
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("NIEWIERNE PSY RULEZ!!!!");
 
+//ukrywanie odpowiednich części
 $("header p, #heroBtns div").hide();
 
+//przejście z pierwszego intro do pierwszego menu
 setTimeout(function(){
-$("header p, #heroBtns div").fadeIn(750);
-$("body").fadeIn(750).css("background", "beige");
-
+  $("header p, #heroBtns div").fadeIn(750);
+  $("body").fadeIn(750).css("background", "beige");
+  $("#mainPart").empty().html(firstMenu.textHello);
 }, 16000);
 
-$("#info").on("click", function(){
-  $("#mainPart").empty().html("<p class='standardText newRocker'>To kolejna wersja gry Audaces, w której wprowadzone zostały wszystkie nie zrealizowane wcześniej pomysły. Gra powstała dzięki użytym technologiom: HTML5, SASS, jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com.</p>");
-});
-
-
-$("#licence").on("click", function(){
-  $("#mainPart").empty().html("<p class='standardText newRocker'>UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji).</p><p class='standardText newRocker'>POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI.</p><p class='standardText newRocker'>Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobbystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audaces jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie może w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.</p>");
-});
+//funkcje dla przycisków pierwszego menu
+$("#info").on("click", () => { firstMenu.infoBtn(); });
+$("#licence").on("click", () => { firstMenu.licenceBtn(); });
+$("#tutorial").on("click", () => { firstMenu.tutorialBtn(); });
 
 });//koniec DOMContentLoaded
