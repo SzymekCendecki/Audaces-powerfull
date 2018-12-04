5ff86e71b3ad3e26913112a1ae29eea128b5d5ca
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-12-04 20:47:51 +0100

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -273,10 +273,145 @@ module.exports.heroCreatorResult = function (hero, skills, equip) {
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-throw new Error("Module build failed: SyntaxError: C:/Users/Asus/Desktop/Audaces - powerfull/js/heroCreator.js: Unexpected token, expected ; (127:7)\n\n  125 | \t\t\t\tconsole.log(weight + \" kg\", height + \" cm\");\n  126 | \t\t\t}else if(race == \"półelf\"){\n> 127 | \t\t\t\tet weight = Math.round(Math.random()*60 + 40);\n      | \t\t\t\t   ^\n  128 | \t\t\t\tlet height = Math.round(Math.random()*45 + 165);\n  129 | \t\t\t\tconsole.log(weight + \" kg\", height + \" cm\");\n  130 | \t\t\t}\n");
+
+
+var functions = __webpack_require__(0);
+
+//tablica zbiorcza z wyniami losowania lub wyborami postaci
+//0-imię, 1-płeć, 2-rasa, 3-profesja, 4-siła, 5-wytrzymałość, 6-zręczność, 7-inteligencja, 8-charyzma, 9-kolor oczu, 10-kolor włosów, 11-kolor skóry, 12 - tatuaże, 13 - waga, 14-wzrost
+var hero = ["nie wybrano", "nie wybrano", "nie wybrano", "nie wybrano", 0, 0, 0, 0, 0, "nie wybrano", "nie wybrano", "nie wybrano", "nie wybrano", "nie wybrano", "nie wybrano"];
+module.exports.hero = hero;
+
+//tablica ekwipunku
+var equip = [" ", " ", " ", " ", " "];module.exports.equip = equip;
+
+//tablica umiejętności
+var skills = [" ", " ", " "];module.exports.skills = skills;
+
+//tablica ze złotem
+var gold = [0];
+
+//tablica z imionami męskimi
+var namesMan = ["Wortigern", "Gintor", "Hegel", "Derig", "Diggramon", "Zengowetoryk", "Deggetm", "Zigamon", "Birduk", "Ardenor", "Winterks", "Joluntik", "Menigor", "Oltis", "Kurdir"];
+
+//tablica z imionami żeńskimi
+var namesWomen = ["Wortigerna", "Hejacynta", "Dejawina", "Ludiniam", "Keoburna", "Leokamina", "Erminia", "Xynenda", "Fejmira", "Apsurginis", "Wicynia", "Jermodernia", "Sertyksa"];
+
+//tablica z rasami
+var races = ["człowiek", "półork", "ork", "półelf", "elf", "krasnolud", "gnom", "niziołek", "goblin", "trol", "półolbrzym"];
+
+//tablica z profesjami
+var occupations = ["wojownik", "złoczyńca", "czarodziej"];
+
+//tabica z płcią
+var sex = ["kobieta", "mężczyzna", "nie wiadomo"];
+
+//tablica z kolorami włosów
+var hairColor = ["blond", "rude", "czarne", "farbowane"];
+
+//tablica z kolorem oczu
+var eyesColor = ["piwne", "szare", "brązowe", "niebieskie"];
+
+//tablica z kolorem skóry
+var skinColor = ["biała", "brązowa", "czarna", "czerwona", "zółta", "zielona", "brunatna", "błękitna"];
+
+//tablica z tatuażami
+var tattoo = ["brak", "więzienne", "plemienne", "dziwne"];
+
+//tablica z wagą
+var weight = ["niedowaga", "normalna", "nadwaga"];
+
+//tablica ze wzrostem
+var height = ["niski", "normalny", "wysoki"];
+
+//tablice z ekwipunkiem
+//broń
+var equipWeapon = ["sztylet", "drewniana pałka", "krótki miecz", "szabla", "włócznia", "proca", "łuk"];
+
+//zbroja
+var equipArmor = ["przeszywanica", "zbroja skórzana", "zbroja ćwiekowana"];
+
+//tarcze
+var equipShield = ["puklerz", "mała tarcza drewniana", "mała tarcza metalowa"];
+
+//inny ekwipunek
+var equipOther = ["kostur", "mieszek", "torba podróżna", "sakwa", "plecak", "manierka", "sagan", "koc", "tuba na perg.", "pęk piór do pis.", "pergaminy 5szt.", "zwykłe ubranie", "fikuśna czapka", "płaszcz", "skórzany pas", "igły i nici", "derka", "namiot", "drewniana miska", "drewniana łyżka", "pochodnia", "lampa oliwna", "kaganek", "lina 5m", "hubka i krzesiwo"];
+
+//tablice z umiejętnościami
+//wojownika
+var skillsWarrior = ["szt.przetrwania", "dyscyplina", "dowodzenie", "uderzenie tarczą", "jeździectwo", "sztylet", "krótki miecz", "szabla", "włócznia", "łuk", "puklerz", "mała tarcza drewniana", "mała tarcza metalowa"];
+module.exports.skillsWarrior = skillsWarrior;
+
+//złoczyńcy
+var skillsCriminal = ["trucizny", "wspinaczka", "aktorstwo", "akrobatyka", "pułapki", "skradanie się", "kradzież", "uniki", "blefowanie", "drewniana pałka"];
+module.exports.skillsCriminal = skillsCriminal;
+
+//czarodzieja
+var skillsWizard = ["pisanie i czytanie", "przyw./odp. demona", "wróżbiarstwo", "leczenie ran", "rzuczanie czarów", "tworz. eliksirów", "tworz.mag. przedm.", "tworzenie maści", "tworzenie runów", "astrologia", "zielarstwo"];
+module.exports.skillsWizard = skillsWizard;
+
+//tablice ze modyfikatorami rasy i profesji - dla określenia ostatecznej ilości punktów postaci
+//indeksy: 0-siła, 1-wytrzymałość, 2-zręczność, 3-inteligencja, 4-charyzma
+var warrior = [5, 5, 0, 0, 0];module.exports.warrior = warrior;
+var criminal = [0, 0, 10, 0, 0];module.exports.criminal = criminal;
+var wizard = [0, 0, 0, 5, 5];module.exports.wizard = wizard;
+
+var human = [0, 0, 0, 0, 0];module.exports.human = human;
+var halfOrc = [3, 3, 0, -3, -3];module.exports.halfOrc = halfOrc;
+var orc = [5, 5, 0, -5, -5];module.exports.orc = orc;
+var halfElv = [-3, -3, 0, 3, 3];module.exports.halfElv = halfElv;
+var elv = [-5, -5, 0, 5, 5];module.exports.elv = elv;
+var dwarf = [4, 4, 0, -2, -3];module.exports.dwarf = dwarf;
+var gnome = [-2, -2, 3, 3, 0];module.exports.gnome = gnome;
+var halfling = [-3, 0, 6, 0, 0];module.exports.halfling = halfling;
+var goblin = [2, -2, 4, 0, -4];module.exports.goblin = goblin;
+var troll = [2, 0, 0, -2, -2];module.exports.troll = troll;
+var semiGiant = [7, 7, -5, -3, 0];module.exports.semiGiant = semiGiant;
+
+module.exports.random = function () {
+	$("#randomHero").on("click", function () {
+		functions.random(sex, 1); //losowanie płci
+		functions.nameRandom(namesWomen, namesMan); //losowanie imienia
+		functions.random(races, 2); //losowanie rasy
+		functions.random(occupations, 3); //losowanie profesji
+		functions.randomPoints2(hero[2], hero[3]); //losowanie punktów
+		functions.random(eyesColor, 9); //losowanie koloru oczu
+		functions.random(hairColor, 10); //losowanie koloru włosów
+		functions.random(skinColor, 11); //losowanie koloru skóry
+		functions.random(tattoo, 12); //losowanie tatuaży
+		functions.random(weight, 13); //losowanie wagi
+		functions.random(height, 14); //losowanie wzrost
+		functions.randomSkills(); //losowanie umiejętności
+		functions.randomEquip(equipWeapon, equipArmor, equipShield, equipOther); //losowanie ekwipunku
+		functions.heroCreatorResult(hero, skills, equip); //wyświetlanie wyniów losowania
+		$("#heroResults span").addClass("greenText");
+
+		function rand(race) {
+			if (race == "człowiek") {
+				var _weight = Math.round(Math.random() * 80 + 40);
+				var _height = Math.round(Math.random() * 60 + 150);
+				console.log(_weight + " kg", _height + " cm");
+			} else if (race == "półork") {
+				var _weight2 = Math.round(Math.random() * 90 + 50);
+				var _height2 = Math.round(Math.random() * 50 + 170);
+				console.log(_weight2 + " kg", _height2 + " cm");
+			} else if (race == "ork") {
+				var _weight3 = Math.round(Math.random() * 100 + 80);
+				var _height3 = Math.round(Math.random() * 70 + 170);
+				console.log(_weight3 + " kg", _height3 + " cm");
+			} else if (race == "półelf") {
+				var _weight4 = Math.round(Math.random() * 60 + 40);
+				var _height4 = Math.round(Math.random() * 45 + 165);
+				console.log(_weight4 + " kg", _height4 + " cm");
+			}
+		}
+
+		rand(hero[2]);
+	});
+};
 
 /***/ }),
 /* 2 */
